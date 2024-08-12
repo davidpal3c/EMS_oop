4aeecf9e6a1ceaa7ab00a52e4b812eda542c9e4e
David Palacios <davidpal3c@gmail.com>
2024-08-12 09:56:48 -0600

    'reports service and component update'

---
+++ Models/EmployeeDirectoryReport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Models
+{
+    public class EmployeeDirectoryReport : Report
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _position;
+        private int _salary;
+        private DateTime _createdAt;
+        private DateTime _updatedAt;
+
+
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+        public string Position { get { return _position; } set { _position = value; } }
+        public int Salary { get { return _salary; } set { _salary = value; } }
+        public DateTime CreatedAt { get { return _createdAt; } set { _createdAt = value; } }
+        public DateTime UpdatedAt { get { return _updatedAt; } set { _updatedAt = value; } }
+
+
+        public EmployeeDirectoryReport()
+        {
+            ReportName = "Employee Directory Report";
+            ReportQuery = EReportType.EmployeeDirectory;
+        }
+               
+
+    }
+}
---
+++ Services/ReportService.cs
@@ -0,0 +1,81 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EMS.Models;
+
+
+namespace EMS.Services
+{
+    public class ReportService
+    {
+        private readonly DBService _dbService;
+        private readonly ReportMapper _reportMapper;
+
+        public ReportService(DBService dbService, ReportMapper reportMapper)
+        {
+            _dbService = dbService ?? throw new ArgumentNullException(nameof(dbService));
+            _reportMapper = reportMapper ?? throw new ArgumentNullException(nameof(reportMapper));
+        }
+
+
+        public async Task<List<Report>> GetReport(string reportType)
+        {
+            List<Report> reportList = new List<Report>();
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(_dbService.ConnectionString))
+                {
+                    await conn.OpenAsync();
+
+                    using (MySqlCommand cmd = new MySqlCommand(reportType, conn))
+                    {
+                        using (MySqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (reportType == "EmployeeDirectory")
+                            {
+                                while (await reader.ReadAsync())
+                                {
+                                    EmployeeDirectoryReport report = _reportMapper.MapFromReaderEmpDirectoryReport(reader);
+                                    reportList.Add(report);
+                                }                                                               
+
+                            }                            
+                            else if (reportType == "Attendance")
+                            {
+                                while (await reader.ReadAsync())
+                                {
+                                    AttendanceReport report = _reportMapper.MapFromReaderAttendanceReport(reader);
+                                    reportList.Add(report);
+                                }
+                            }                            
+                            else if (reportType == "LeaveAndAbsence")
+                            {
+                                while (await reader.ReadAsync())
+                                {
+                                    LeaveAndAbsenceReport report = _reportMapper.MapLeaveAndAbsenceReport(reader);
+                                    reportList.Add(report);
+                                }
+                            }                            
+                            else
+                            {
+                                throw new Exception("Invalid report type");
+                            }
+
+                            return reportList;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error fetching payrolls: {ex.Message}");
+            }
+            
+        }
+    }
+}
